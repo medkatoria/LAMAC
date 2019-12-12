3bd2c4863c6556830d03efe4f715d6ec8c2d9f84
medkatoria <47912358+medkatoria@users.noreply.github.com>
2019-12-12 22:25:58 +0100

    f

--- contact.php
+++ contact.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
   <head>
     <title>La Machine à Coudre</title>
     <meta charset="UTF-8">
@@ -21,6 +21,56 @@
   </style>
 </head>
 
+<?php
+
+include "settings.php";
+
+$nomErr = $mailErr = $messageErr = $prenomErr = $phoneErr = "";
+$nom = $mail = $prenom = $message = $phone = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["nom"])) {
+    $nomErr = "Une indication du nom est requise.";
+  }  else {
+    $nom = test_imput($_POST["nom"]);
+    if (!preg_match("/^[a-zA-Z  ]*$/",$nom)) {
+      $nomErr = "Seuls les lettres et les espaces sont autorisés.";
+    }
+  }
+  if (empty($_POST["prenom"])) {
+    $prenomErr = "Une indication du nom est requise.";
+  }  else {
+    $prenom = test_imput($_POST["prenom"]);
+    if (!preg_match("/^[a-zA-Z  ]*$/",$prenom)) {
+      $prenomErr = "Seuls les lettres et les espaces sont autorisés.";
+    }
+  }
+  if (empty($_POST["mail"])) {
+    $mailErr = "Une adresse email est requise.";
+  } else {
+    $mail = test_input($_POST["mail"]);
+    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+      $mailErr = "Le format de l'adresse email est incorrect.";
+    }
+  }
+  if (empty($_POST["phone"])) {
+    $phoneErr = "Le numéro de téléphone est nécessaire.";
+  } else {
+    $phone = test_imput($_POST["phone"]);
+    if (!preg_match("[-a-zA-Z]",$phone)) {
+        $phone = "Le numéro de téléphone est erroné.";
+    }
+  }
+  if (empty($_POST["message"])) {
+    $messageErr = "Le message est vide.";
+  } else {
+    $message = test_imput($_POST["message"]);
+    }
+  }
+
+
+
+?>
 
 <body class="w3-light-grey">
 
@@ -121,58 +171,6 @@
     </div>
   </div>
 </div>
-
-<?php
-$nomErr = $mailErr = $messageErr = $prenomErr = $phoneErr = "";
-$nom = $mail = $prenom = $message = $phone = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["nom"])) {
-    $nomErr = "Une indication du nom est requise.";
-  }  else {
-    $nom = test_imput($_POST["nom"]);
-    if (!preg_match("/^[a-zA-Z  ]*$/",$nom)) {
-      $nomErr = "Seuls les lettres et les espaces sont autorisés.";
-    }
-  }
-  if (empty($_POST["prenom"])) {
-    $prenomErr = "Une indication du nom est requise.";
-  }  else {
-    $prenom = test_imput($_POST["prenom"]);
-    if (!preg_match("/^[a-zA-Z  ]*$/",$prenom)) {
-      $prenomErr = "Seuls les lettres et les espaces sont autorisés.";
-    }
-  }
-  if (empty($_POST["mail"])) {
-    $mailErr = "Une adresse email est requise.";
-  } else {
-    $mail = test_input($_POST["mail"]);
-    if (!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
-      $mailErr = "Le format de l'adresse email est incorrect.";
-    }
-    }
-    if (empty($_POST["phone"])) {
-      $phoneErr = "Le numéro de téléphone est nécessaire.";
-    } else {
-      $phone = test_imput($_POST["phone"]);
-      if (!preg_match("[-a-zA-Z]",$phone)) {
-        $phone = "Le numéro de téléphone est erroné.";
-      }
-    }
-    if (empty($_POST["message"])) {
-      $messageErr = "Le message est vide.";
-    } else {
-        $message = test_imput($_POST["message"]);
-      }
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
 
 <!-- footer -->
 <footer class="w3-container w3-content w3-green w3-margin-top w3-center" style="max-width:1200px;">
